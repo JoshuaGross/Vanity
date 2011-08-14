f07d2201bf88eed2f9aa42a901b5b4d766518aa8
Joshua Gross <joshua.gross@gmail.com>
2011-08-14 02:37:20 -0700

    Default admit password and warning message

--- vanity.js
+++ vanity.js
@@ -1,12 +1,14 @@
-/**
- * Module dependencies.
- */
+// Config
+var resume_path = '/resources/resume.pdf';
+var password = 'password';
 
+// Module dependencies.
 var express = require('express');
+var fs = require('fs');
 var app = module.exports = express.createServer();
 var port;
 
-// Configuration
+// Express App Configuration
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
@@ -41,6 +43,7 @@ app.get('/', function(req, res){
     html_head_ifs: '<!--[if lt IE 7]><html class="ie ie6" lang="en"><[endif]--><!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]--><!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]--><!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->',
     title: 'josh is gross',
     tagline: 'developer, third culture kid, human',
+    resume_path: resume_path,
     contact: {
       email: 'joshua.gross@gmail.com',
       facebook: 'http://www.facebook.com/joshisgross',
@@ -64,3 +67,18 @@ app.get('/', function(req, res){
 port = process.env.PORT || port;
 app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+// Help Vanity site owner with some setup
+var resume_exists = false;
+try {
+  resume_exists = fs.lstatSync('public'+resume_path).isFile();
+} catch (e) {
+  console.log(e);
+}
+if (!resume_exists) {
+  console.log('WARNING: no resume file found. Place it at /public'+resume_path);
+}
+
+if (password == 'password') {
+  console.log('WARNING: you really should change the default password!');
+}
